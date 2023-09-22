8fc637198a798af403597fb6d43d38ac2b43879b
Jay/Fienna Liang <roackb2@gmail.com>
2023-09-22 23:53:29 +0800

    Clean up duplicate files

--- src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <span className="text-4xl">404 Not Found</span>
-    </div>
-  )
-}
